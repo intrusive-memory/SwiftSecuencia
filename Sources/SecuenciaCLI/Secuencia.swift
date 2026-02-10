ce3fb1372dccbb49381bc2ba2ebf7a8513bc6a43
Tom Stovall <119924+stovak@users.noreply.github.com>
2026-02-09 17:33:12 -0800

    Add Secuencia main entry point

---
+++ Sources/SecuenciaCLI/Secuencia.swift
@@ -0,0 +1,12 @@
+import ArgumentParser
+import Foundation
+
+@main
+struct Secuencia: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "secuencia",
+        abstract: "A command-line tool for generating Final Cut Pro timelines from JSON definitions.",
+        version: "1.0.0",
+        subcommands: [Build.self]
+    )
+}
