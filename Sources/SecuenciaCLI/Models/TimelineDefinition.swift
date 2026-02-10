263c748716ac39a22c45aae99b095f2ca6033745
Tom Stovall <119924+stovak@users.noreply.github.com>
2026-02-09 17:33:42 -0800

    Add timeline configuration structs

--- Sources/SecuenciaCLI/Models/TimelineDefinition.swift
+++ Sources/SecuenciaCLI/Models/TimelineDefinition.swift
@@ -6,6 +6,27 @@ struct TimelineDefinition: Codable, Sendable {
     let clips: [ClipDefinition]
 }
 
+/// Timeline configuration.
+struct TimelineConfig: Codable, Sendable {
+    let name: String
+    let format: FormatConfig
+    let audio: AudioConfig
+}
+
+/// Video format configuration.
+struct FormatConfig: Codable, Sendable {
+    let width: Int
+    let height: Int
+    let frameRate: String
+    let colorSpace: String?
+}
+
+/// Audio configuration.
+struct AudioConfig: Codable, Sendable {
+    let layout: String
+    let rate: String
+}
+
 /// Type of clip in the timeline.
 enum ClipType: String, Codable, Sendable {
     case video
